578fd21d5c0c89dacde779e12b4c264afeb42dbc
lyj <742791089@qq.com>
2020-01-05 21:50:35 +0800

    push

--- jiafuhou.py
+++ jiafuhou.py
@@ -5,17 +5,14 @@ import uiautomator2
 # 驱动器 控制器
 d = uiautomator2.connect_usb('127.0.0.1:62001')
 d2 = uiautomator2.connect_usb('127.0.0.1:62025')
-d3 = uiautomator2.connect_usb('127.0.0.1:62026')
+# d3 = uiautomator2.connect_usb('127.0.0.1:62026')
 d4 = uiautomator2.connect_usb('127.0.0.1:62027')
-# d5 = uiautomator2.connect_usb('127.0.0.1:62028')
-# d6 = uiautomator2.connect_usb('127.0.0.1:62029')
+d5 = uiautomator2.connect_usb('127.0.0.1:62028')
 
 # 当前时间
 now_time = time.localtime()
 # 目标时间(电脑时间比北京时间快了5/6秒) 电脑休眠模拟器时间不会走 所以要提前设置
-target_time = time.strptime("2019-09-29 19:30:12", "%Y-%m-%d %H:%M:%S")
-
-# 距离目标的点击时间
+target_time = time.strptime("2019-10-30 19:30:00", "%Y-%m-%d %H:%M:%S")
 diff = (target_time.tm_hour - now_time.tm_hour) * 60 * 60 + (
             target_time.tm_min - now_time.tm_min) * 60 + (
                          target_time.tm_sec - now_time.tm_sec)
@@ -26,13 +23,11 @@ print(time.localtime())
 count = 1;
 while count < 3:
  d.xpath('//*[@text="領養"]').click()
- # d5.xpath('//*[@text="領養"]').click()
+ d5.xpath('//*[@text="領養"]').click()
  d4.xpath('//*[@text="領養"]').click()
  d2.xpath('//*[@text="領養"]').click()
- d3.xpath('//*[@text="領養"]').click()
+ # d3.xpath('//*[@text="領養"]').click()
 
-# d5.xpath('//*[@text="領養"]').click()
-# d6.xpath('//*[@text="領養"]').click()
  count = count + 1
 print("finish click 19:30")
 
